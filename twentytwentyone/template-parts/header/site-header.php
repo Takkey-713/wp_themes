0733e6f0c95689c45cee959ad9852c6c292c05f2
Takkey-713 <takiyu713@gmail.com>
2023-12-22 16:13:53 +0900

    deploy test

--- twentytwentyone/template-parts/header/site-header.php
+++ twentytwentyone/template-parts/header/site-header.php
@@ -14,7 +14,7 @@ $wrapper_classes .= has_nav_menu( 'primary' ) ? ' has-menu' : '';
 ?>
 
 <header id="masthead" class="<?php echo esc_attr( $wrapper_classes ); ?>">
-	<div>deploy test2</div>
+	<div>deploy test3</div>
 	<?php get_template_part( 'template-parts/header/site-branding' ); ?>
 	<?php get_template_part( 'template-parts/header/site-nav' ); ?>
 
